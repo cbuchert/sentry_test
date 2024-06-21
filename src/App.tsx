dfdda95086bde0cfeea40ac73e59176b42ceef80
Chris <christianbuchert@gmail.com>
2024-06-21 13:19:10 -0600

    When the count hits 3, throw an error to test whether or not Sentry is working.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 function App() {
-  return <div>Sanity check</div>;
+  const [count, setCount] = useState(0);
+
+  if (count === 3) {
+    throw new Error("Hey, Sentry! Are you there?");
+  }
+
+  return (
+    <div>
+      <button onClick={(e) => setCount((previousCount) => previousCount + 1)}>
+        {count}
+      </button>
+    </div>
+  );
 }
 
 export default App;
