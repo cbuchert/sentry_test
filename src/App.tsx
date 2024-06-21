12cfc67697c06441cc5ac690da45da7d38548ed4
Chris <christianbuchert@gmail.com>
2024-06-21 13:43:09 -0600

    Fixed a linting error.

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,6 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  if (count === 3) {
-    throw new Error("Hey, Sentry! Are you there?");
-  }
-
   return (
     <div>
       <button onClick={() => setCount((previousCount) => previousCount + 1)}>
